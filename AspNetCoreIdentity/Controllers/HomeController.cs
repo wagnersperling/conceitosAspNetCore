21101cbef3f468029744da7a2ab6c2b36b1fd351
Carlos Wagner <carlos.sperling@granter.com.br>
2021-08-07 08:05:16 -0300

    Customizado autenticação.

--- AspNetCoreIdentity/Controllers/HomeController.cs
+++ AspNetCoreIdentity/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using AspNetCoreIdentity.Models;
+﻿using AspNetCoreIdentity.Extensions;
+using AspNetCoreIdentity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -37,6 +38,12 @@ namespace AspNetCoreIdentity.Controllers
 
         [Authorize(Policy = "PodeEscrever")]
         public IActionResult SecretClaimGravar()
+        {
+            return View("Secret");
+        }
+
+        [ClaimsAuthorize("Produtos", "Ler")]
+        public IActionResult ClaimsCustom()
         {
             return View("Secret");
         }
